f08a78872a6241f747cbb1223f8f4006e6c0a1f5
rerichaa <adam.rericha@gmail.com>
2024-05-19 18:48:29 +0200

    Add relative addressing

--- src/main/java/cz/cuni/mff/rerichaa/ed/Command.java
+++ src/main/java/cz/cuni/mff/rerichaa/ed/Command.java
@@ -9,45 +9,47 @@ public class Command {
 
     public Command(String sCommand, int currLine, int lastLine){
         enum State {
-            RANGE1,
-            RANGE2,
+            RANGELOW,
+            RANGEHIGH,
+            PLUS,
+            MINUS,
             DESTINATION,
             ARGUMENT
         }
         name = ' ';
         char[] chars = sCommand.toCharArray();
 
-        State state = State.RANGE1;
+        State state = State.RANGELOW;
 
         StringBuilder sb = new StringBuilder();
+        boolean fromHigh = false;
 
         int rangeLower = -1;
         for (char ch : chars){
             switch (state){
-                case RANGE1:
+                case RANGELOW:
                     if (Character.isDigit(ch)){
                         sb.append(ch);
                     } else if (ch == ',') {
                         if (!sb.isEmpty()){
                             rangeLower = Integer.parseInt(sb.toString());
+                            range = new Range(rangeLower, rangeLower);
                         }
                         else {
                             if (lastLine > 0)
                                 range = new Range(1 , lastLine);
                         }
-                        state = State.RANGE2;
+                        state = State.RANGEHIGH;
                         sb = new StringBuilder();
                     }else if (ch == '.'){
                         sb.append(Integer.toString(currLine));
                     } else if (ch == '$'){
                         sb.append(Integer.toString(lastLine));
                     } else if (ch == '+' ){
-                        sb.append(Integer.toString(currLine + 1)).append("+");
-
-
+                        state = State.PLUS;
                     }
                     else if (ch == '-'){
-                        sb.append(Integer.toString(currLine-1));
+                        state = State.MINUS;
                     }
                     else if (Character.isLetter(ch)){
                         // only one number in range
@@ -64,7 +66,90 @@ public class Command {
                     }
 
                     break;
-                case RANGE2:
+                case PLUS:
+                    if (Character.isDigit(ch)){
+                        sb.append(ch);
+                    }
+                    else if (Character.isLetter(ch)){
+                        int rangeHigher = currLine +1;
+                        if (!sb.isEmpty()){
+                            rangeHigher += Integer.parseInt(sb.toString()) -1;
+                            sb = new StringBuilder();
+                        }
+                        if (fromHigh)
+                            range = new Range(rangeLower,rangeHigher);
+                        else{
+                            rangeLower = rangeHigher;
+                            range = new Range(rangeHigher, rangeHigher);
+                        }
+
+                        name = ch;
+                        state = State.DESTINATION;
+                    }
+                    else if (ch == ','){
+                        int rangeHigher = currLine +1;
+
+                        if (!sb.isEmpty()){
+                            rangeHigher += Integer.parseInt(sb.toString()) -1;
+                            sb = new StringBuilder();
+                        }
+                        if (fromHigh)
+                            range = new Range(rangeLower,rangeHigher);
+                        else{
+                            rangeLower = rangeHigher;
+                            range = new Range(rangeHigher, rangeHigher);
+                        }
+
+                        state = State.RANGEHIGH;
+                    }
+                    else{
+                        System.out.println("!");
+                    }
+
+                    break;
+                case MINUS:
+                    if (Character.isDigit(ch)){
+                        sb.append(ch);
+                    }
+                    else if (Character.isLetter(ch)){
+                        int rangeHigher = currLine-1;
+                        if (!sb.isEmpty()){
+                            rangeHigher -= Integer.parseInt(sb.toString()) -1;
+
+                            sb = new StringBuilder();
+
+                        }
+                        if (fromHigh)
+                            range = new Range(rangeLower,rangeHigher);
+                        else{
+                            rangeLower = rangeHigher;
+                            range = new Range(rangeHigher, rangeHigher);
+                        }
+
+                        name = ch;
+                        state = State.DESTINATION;
+                    }
+                    else if (ch == ','){
+                        int rangeHigher = currLine -1;
+
+                        if (!sb.isEmpty()){
+                            rangeHigher -= Integer.parseInt(sb.toString()) -1;
+                            sb = new StringBuilder();
+                        }
+                        if (fromHigh)
+                            range = new Range(rangeLower,rangeHigher);
+                        else{
+                            rangeLower = rangeHigher;
+                            range = new Range(rangeHigher, rangeHigher);
+                        }
+
+                        state = State.RANGEHIGH;
+                    }
+                    else{
+                        System.out.println("!");
+                    }
+                    break;
+                case RANGEHIGH:
                     if (Character.isDigit(ch)){
                         sb.append(ch);
                     } else if (Character.isLetter(ch)){
@@ -79,6 +164,13 @@ public class Command {
                         sb.append(Integer.toString(currLine));
                     } else if (ch == '$'){
                         sb.append(Integer.toString(lastLine));
+                    } else if (ch == '+'){
+                        fromHigh = true;
+                        state = State.PLUS;
+
+                    } else if (ch == '-'){
+                        fromHigh = true;
+                        state = State.MINUS;
                     }
                     break;
                 case DESTINATION:
@@ -105,8 +197,23 @@ public class Command {
             }
 
         }
-        if (state == State.RANGE1 && !sb.isEmpty())
+        if (state == State.RANGELOW && !sb.isEmpty())
             range = new Range(Integer.parseInt(sb.toString()), Integer.parseInt(sb.toString()));
+        else if (state == State.MINUS || state == State.PLUS){
+
+            if (!sb.isEmpty()){
+                int num = Integer.parseInt(sb.toString());
+                if (state == State.PLUS)
+                    range = new Range(currLine + num, currLine + num);
+                else
+                    range = new Range(currLine - num, currLine - num);
+            }
+            else
+                if (state == State.PLUS)
+                    range = new Range(currLine+1, currLine+1);
+                else
+                    range = new Range(currLine-1, currLine-1);
+        }
 
         else if (state == State.ARGUMENT && !sb.isEmpty())
             argument = sb.toString();
@@ -115,6 +222,7 @@ public class Command {
             destinationLine = Integer.parseInt(sb.toString());
 
     }
+
     public String toString(){
         return String.format("%s %s %d %s", range, name, destinationLine, argument);
     }
